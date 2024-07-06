5efc6bb50e61732ae25ef89abab4bdb67250fddb
Ian Beckett <redacted>
2024-07-06 15:42:03 -0500

    add react with vite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const Card = (props) => {
   return React.createElement(
@@ -52,5 +52,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
