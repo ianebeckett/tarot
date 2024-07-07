58b47a0cb8527953a2faa144691fd6787307b21e
Ian Beckett <redacted>
2024-07-06 19:28:32 -0500

    rename card images

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ const App = () => {
     <div>
       <h1>Tarot Reading</h1>
       <div className="reading-table">
-        <Card art="./cards/512px-CardBack.jpg" />
-        <Card art="/cards/512px-Swords07.jpg" />
-        <Card art="/cards/512px-Swords08.jpg" />
-        <Card art="/cards/512px-RWS_Tarot_21_World.jpg" />
+        <Card art="./cards/CardBack.jpg" />
+        <Card art="/cards/Swords07.jpg" />
+        <Card art="/cards/Swords08.jpg" />
+        <Card art="/cards/21_World.jpg" />
       </div>
     </div>
   );
