88c4f1e5a388498678748bffcd74cceac3c3c37d
Ian Beckett <redacted>
2024-07-12 19:23:26 -0500

    format

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,44 @@ import { createRoot } from "react-dom/client";
 import allCards from "./allCards.json";
 
 const App = () => {
-    let deck = allCards.images;
-    const [cards, setCards] = useState(["/cards/00_CardBack.jpg"]);
+  let deck = allCards.images;
+  const [cards, setCards] = useState(["/cards/00_CardBack.jpg"]);
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-    function handleDraw() {
-        const images = allCards.images;
-        let random;
-        do {
-            random = getRandomInt(0, images.length - 1);
-        } while (cards.includes(images[random]));
-        setCards((prevCards) => [...prevCards, deck[random]]);
-        console.log(cards);
-    };
+  function handleDraw() {
+    const images = allCards.images;
+    let random;
+    do {
+      random = getRandomInt(0, images.length - 1);
+    } while (cards.includes(images[random]));
+    setCards((prevCards) => [...prevCards, deck[random]]);
+    console.log(cards);
+  }
 
-    return (
-        <div className="margin">
-            <div className="control-panel">
-                <h1>Tarot Reading</h1>
-                <button onClick={handleDraw} className="button-65">
-                    draw a card
-                </button>
+  return (
+    <div className="margin">
+      <div className="control-panel">
+        <h1>Tarot Reading</h1>
+        <button onClick={handleDraw} className="button-65">
+          draw a card
+        </button>
+      </div>
+      <div className="cardTable">
+        {cards.map((card, index) => {
+          return (
+            <div key={index} className="card">
+              <img src={card} />
             </div>
-            <div className="cardTable"> {
-                cards.map((card, index) => {
-                    return (
-                        <div key={index} className="card" >
-                            <img src={card} />
-                        </div>
-                    )
-                })}
-            </div>
-        </div >
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
