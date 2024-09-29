2358f21ed1ae12513eaa00d9fa3e68c7793fa399
Ian Beckett <redacted>
2024-09-28 19:44:59 -0500

    move deck to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,18 @@
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-import allCards from "./allCards.json";
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { Deck } from "./Deck"
 
-const App = () => {
-    let deck = allCards.images;
-    let backArt = "/cards/00_CardBack.jpg";
-    const [cards, setCards] = useState([backArt, backArt]);
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    function handleDraw() {
-        if (cards.length === 5) {
-            return;
-        }
-
-        const images = allCards.images;
-        let random;
-        do {
-            random = getRandomInt(0, images.length - 1);
-        } while (cards.includes(images[random]));
-        setCards((prevCards) => [...prevCards, deck[random]]);
-    }
-
-    function setClass(i) {
-        if (i == 0) {
-            return "deck";
-        }
-        if (i == 1) {
-            return "top-card";
-        }
-        return "card";
-    }
-
-    function setCardOnClick(i) {
-        if (i == 0) {
-            return null;
-        }
-        if (i == 1) {
-            return handleDraw;
-        }
-        return showcase;
-    }
-
-    function handleReset() {
-        setCards([backArt, backArt]);
-    }
-
-    function showcase() {
-        //console.log("yay");
-    }
-
+function App() {
     return (
-        <div>
-            <div className="card-table">
-                {cards.map((card, index) => {
-                    return (
-                        <div key={index} className={setClass(index)}>
-                            <img src={card} onClick={setCardOnClick(index)} />
-                        </div>
-                    );
-                })}
-            </div>
-            <div className='btn' id='btn-reset' onClick={handleReset}>
+        <div className="card-table">
+            <Deck />
+            <div className='btn' id='btn-reset' onClick={null}>
                 <button >RESET</button>
             </div>
         </div>
     );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(React.createElement(App));
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(React.createElement(App))
---
+++ src/Card.jsx
@@ -0,0 +1,4 @@
+function showcase() {
+    //console.log("yay");
+}
+
